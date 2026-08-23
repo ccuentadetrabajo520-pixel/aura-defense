f66806f516a6e08a4cd1c6b7242198e1212214ca
ccuentadetrabajo61-hash <ccuentadetrabajo61@gmail.com>
2026-08-23 17:17:56 +0000

    Fix: Definitively resolve pnpm and Metro issues for Codemagic build

--- artifacts/mobile/metro.config.js
+++ artifacts/mobile/metro.config.js
@@ -1,13 +1,18 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
-
 const config = getDefaultConfig(__dirname);
-
-// Fix for pnpm and monorepo symlinks
 config.resolver.nodeModulesPaths = [
   path.resolve(__dirname, 'node_modules'),
   path.resolve(__dirname, '../node_modules'),
+  path.resolve(__dirname, '../../node_modules'),
+];
+config.resolver.extraNodeModules = {
+  react: path.resolve(__dirname, '../../node_modules/react'),
+};
+config.resolver.unstable_enableSymlinks = true;
+config.watchFolders = [
+  path.resolve(__dirname, '..'),
+  path.resolve(__dirname, '../..'),
 ];
 config.resolver.disableHierarchicalLookup = false;
-
 module.exports = config;
